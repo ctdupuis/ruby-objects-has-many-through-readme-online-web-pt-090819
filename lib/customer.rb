c0e1c91c4abd4b6876e28b4995c0bd68365d486a
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-03 17:32:27 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,7 +13,7 @@ class Customer
     @@all
   end
   
-  def new_meal(waiter, total, tip=0) #pass customer in as self, that way it refers to the specific instance.
+  def new_meal(waiter, total, tip=0) #pass customer in as self, that way it refers to the specific instance. Through this, you can create a new meal as a customer and have all the attibutes associated to the Customer instance
     Meal.new(waiter, self, total, tip)
   end
 end
