21a86af5ea0944b5d17b8a1ba556840700b67435
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-03 17:39:48 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,4 +16,12 @@ class Waiter
   def new_meal(customer, total, tip) #same as Customer instance, only swapping self to the Waiter's spot
     Meal.new(self, customer, total, tip)
   end
+  
+  def meals #Same as Customer instance
+    Meal.all.select{|meal| meal.waiter == self}
+  end
+  
+  def best_tipper
+    max_tip = meals.max { |meal_a, meal_b| meal_a.tip <=> meal_b.tip }
+    max_tip
 end
