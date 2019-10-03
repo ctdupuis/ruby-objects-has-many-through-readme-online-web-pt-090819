6fc994c1db05c7613978f9ef722534ac7c466c43
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-03 17:26:12 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -4,4 +4,13 @@ class Waiter
   @@all = []
   
   def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
 end
